0222758dc19b0c140c29a9d9313a1f0056d35de9
rgkalden <rgkalden@gmail.com>
2022-04-06 16:45:02 -0600

    Generate new data, update data_visualization

--- data_generation/data_generation.py
+++ data_generation/data_generation.py
@@ -37,6 +37,36 @@ def generate_dataset(params, t_eval, base_dict_1, lim_dict_1, base_dict_2, lim_d
     df.to_csv(filename, index=False)
     print('Dataset saved as ' + filename)
 
+# # Reaction chemistry parameters
+# params = {'V': 20,
+#          'E2': 14000,
+#          'k2c_const': 40,
+#          'y': 1,
+#          'R': 1.987,
+#          'To': 300,
+#          'E1': 8000,
+#          'Cto': 0.2,
+#          'Cpco': 18.02,
+#          'm': 10,
+#          'k1a_const': 40,
+#          'DH1b': -10000,
+#          'DH2a': -8000,
+#          'Cpd': 16,
+#          'Cpa': 10,
+#          'Cpb': 12,
+#          'Cpc': 14,
+#          'Ua': 80}
+# t_eval = np.linspace(0, params['V'], params['V'] + 1)
+
+# # Process condition parameters
+# # Mode 1 parameters
+# base_dict_1 = {'Fa': 5, 'Fb': 10, 'P': 4.92, 'To': 300, 'm': 10, 'Ta': 325}
+# lim_dict_1 = {'Fa': 0.1, 'Fb': 0.1, 'P': 0.1, 'To': 0.03, 'm': 0.2, 'Ta': 0.03}
+
+# # Mode 2 parameters
+# base_dict_2 = {'Fa': 3, 'Fb': 4, 'P': 10, 'To': 500, 'm': 5, 'Ta': 325}
+# lim_dict_2 = {'Fa': 0.05, 'Fb': 0.05, 'P': 0.05, 'To': 0.015, 'm': 0.1, 'Ta': 0.015}
+
 # Set parameters for dataset size
 # num_samples = 10000
 # mode_1_frac = 0.6
